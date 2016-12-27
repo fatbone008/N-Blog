26e1037cafad4a6fc85024346595e214f6e04302
Willian Chan <165080496@qq.com>
2016-12-27 16:10:21 +0800

    routes/posts.js

---
+++ routes/posts.js
@@ -0,0 +1,44 @@
+/**
+ * Created by chenyihui on 2016/12/27.
+ */
+var express = require('express');
+var router = express.Router();
+
+var checkLogin = require('../middlewares/check').checkLogin;
+
+//GET /posts article pages for users or specific users.
+//  eg: GET /posts?author=xxx
+router.get('/',function (req, res, next) {
+    res.send(req.flash());
+});
+
+//POST /posts post an article
+router.post('/',checkLogin,function (req, res, next) {
+    res.send(req.flash());
+});
+
+//GET /posts/create page for create article
+router.get('/create',checkLogin,function (req, res, next) {
+    res.send(req.flash());
+});
+
+//GET /posts/:postId page for single article
+router.get('/:postId',function (res,req,next) {
+    res.send(req.flash());
+});
+
+//GET /posts/:postId/edit page for update article
+router.get('/:postId/edit',checkLogin,function (req, res, next) {
+    res.send(req.flash());
+});
+
+//POST /posts/:postId/edit
+router.post('/:postId/edit',checkLogin,function (req, res, next) {
+    res.send(req.flash());
+});
+
+//GET /posts/:postId/remove
+
+router.get('/:postId/remove',checkLogin,function (req, res, next) {
+    res.send(req.flash());
+});
